322825a5b214cf0e40d30e0983a9d1ad6e54f970
jevinus <jevinus@protonmail.com>
2024-04-22 01:08:10 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const calcResult = document.getElementsByClassName("calculation-result");
+
+const buttons = {
+  0: document.getElementsByClassName("0"),
+  1: document.getElementsByClassName("1"),
+  2: document.getElementsByClassName("2"),
+  3: document.getElementsByClassName("3"),
+  4: document.getElementsByClassName("4"),
+  5: document.getElementsByClassName("5"),
+  6: document.getElementsByClassName("6"),
+  7: document.getElementsByClassName("7"),
+  8: document.getElementsByClassName("8"),
+  9: document.getElementsByClassName("9"),
+  times: document.getElementsByClassName("*"),
+  minus: document.getElementsByClassName("-"),
+  plus: document.getElementsByClassName("+"),
+  dot: document.getElementsByClassName("."),
+  equals: document.getElementsByClassName("="),
+};
