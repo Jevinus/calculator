a68b4ee9609f8c7c79561e91d6a5c537d8bb9e52
Jevinus <jevinus@protonmail.com>
2024-04-24 01:05:35 -0400

    Added operators object

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
+let displayValue = document.querySelector("");
+
+const operators = {
+  addition: function (num1, num2) {
+    return num1 + num2;
+  },
+  subtraction: function (num1, num2) {
+    return num1 - num2;
+  },
+  multiply: function (num1, num2) {
+    return num1 * num2;
+  },
+  divide: function (num1, num2) {
+    return num1 / num2;
+  },
+};
+
+function operate(operator, num1, num2) {}
+
+//
 const calcResult = document.getElementsByClassName("calculation-result");
 
-const buttons = {
-  0: document.getElementsByClassName("0"),
-  1: document.getElementsByClassName("1"),
-  2: document.getElementsByClassName("2"),
-  3: document.getElementsByClassName("3"),
-  4: document.getElementsByClassName("4"),
-  5: document.getElementsByClassName("5"),
-  6: document.getElementsByClassName("6"),
-  7: document.getElementsByClassName("7"),
-  8: document.getElementsByClassName("8"),
-  9: document.getElementsByClassName("9"),
-  times: document.getElementsByClassName("*"),
-  minus: document.getElementsByClassName("-"),
-  plus: document.getElementsByClassName("+"),
-  dot: document.getElementsByClassName("."),
-  equals: document.getElementsByClassName("="),
-};
+// Ignore for now
+// const buttons = {
+//   0: document.getElementsByClassName("0"),
+//   1: document.getElementsByClassName("1"),
+//   2: document.getElementsByClassName("2"),
+//   3: document.getElementsByClassName("3"),
+//   4: document.getElementsByClassName("4"),
+//   5: document.getElementsByClassName("5"),
+//   6: document.getElementsByClassName("6"),
+//   7: document.getElementsByClassName("7"),
+//   8: document.getElementsByClassName("8"),
+//   9: document.getElementsByClassName("9"),
+//   times: document.getElementsByClassName("*"),
+//   minus: document.getElementsByClassName("-"),
+//   plus: document.getElementsByClassName("+"),
+//   dot: document.getElementsByClassName("."),
+//   equals: document.getElementsByClassName("="),
+// };
